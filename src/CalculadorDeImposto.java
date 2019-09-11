e07507106aee731d23da59e9a55c52da0924443a
Thiago Gomes <thiago.gomes3@globalweb.com.br>
2019-09-11 11:18:21 -0300

    1.1 padrão Strategy

---
+++ src/CalculadorDeImposto.java
@@ -0,0 +1,11 @@
+
+public class CalculadorDeImposto {
+
+	public void realizaCalculo (Orcamento orcamento, Imposto imposto) {
+
+		double total = imposto.calcula(orcamento);
+		System.out.println(total);
+	}
+
+
+}
