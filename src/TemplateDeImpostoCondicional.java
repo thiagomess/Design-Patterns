391a9f4e403ed396f996f92f44ce387717965e96
Thiago Gomes <thiago.gomes3@globalweb.com.br>
2019-09-11 12:54:09 -0300

    1.4 Decorator

--- src/TemplateDeImpostoCondicional.java
+++ src/TemplateDeImpostoCondicional.java
@@ -3,15 +3,15 @@
  * @Author Thiago G.
  * 11 de set de 2019
  */
-public abstract class TemplateDeImpostoCondicional implements Imposto {
+public abstract class TemplateDeImpostoCondicional extends Imposto {
 
 	@Override
 	public double calcula(Orcamento orcamento) {
-		if (deveUsarMaximaTaxacao(orcamento)) {
-			return maximaTaxacao(orcamento);
+		if (deveUsarMaximaTaxacao(orcamento) ) {
+			return maximaTaxacao(orcamento) + calculoDeOutroImposto(orcamento);
 		}
 		else {
-			return minimaTaxacao(orcamento);
+			return minimaTaxacao(orcamento) + calculoDeOutroImposto(orcamento);
 		}
 	}
 
