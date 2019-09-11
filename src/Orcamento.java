a62e35049981faa5ec6606fb359648b5c7a14a0f
Thiago Gomes <thiago.gomes3@globalweb.com.br>
2019-09-11 11:53:28 -0300

    1.2 Chain of Responsibility

--- src/Orcamento.java
+++ src/Orcamento.java
@@ -1,14 +1,27 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Orcamento {
 
-	private double valor;
+	private final double valor;
+	private final List<Item> itens;
 
 	public Orcamento(double valor) {
 		this.valor = valor;
+		itens = new ArrayList<>();
 	}
 
 	public double getValor() {
 		return valor;
+	}
+
+	public List<Item> getItens() {
+		return Collections.unmodifiableList(itens) ;
+	}
+	
+	public void adiciona(Item item) {
+		itens.add(item);
 	}
 	
 	
---
+++ src/TesteDeDescontos.java
@@ -0,0 +1,24 @@
+
+/**
+ * @Author Thiago G.
+ * 11 de set de 2019
+ */
+public class TesteDeDescontos {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		CalculadorDeDescontos descontos = new CalculadorDeDescontos();
+		
+		Orcamento orcamento = new Orcamento(400.0);
+		orcamento.adiciona(new Item("Caneta", 250.0));
+		orcamento.adiciona(new Item("Lapis", 250.0));
+		
+		double calcula = descontos.calcula(orcamento);
+		
+		System.out.println(calcula);
+	}
+
+}
